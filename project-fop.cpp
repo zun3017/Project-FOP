8b11a84d98bcc30ff0eabf517bdfe84d056ba91b
zun3017 <hodung300107@gmail.com>
2026-03-21 10:00:29 +0700

    update code

--- project-fop.cpp
+++ project-fop.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+﻿#include<iostream>
 #include<print>
 #include<iomanip>
 #include<string>
@@ -19,13 +19,104 @@ void print(string** arr, mapsize a)
 	{
 		for (int j = 0; j < a.col; j++)
 		{
-			cout << arr[i][j] << " ";
+			cout <<setw(3)<< arr[i][j];
 		}
 		cout << endl;
 	}
 }
-void creatmap(string**& arr, mapsize a)
+void creatmap(string prompt ,string**& arr, mapsize a)
 {
+	int size = 0;
+	int choice = 0;
+	cout << prompt << endl;
+	do
+	{
+		cout << "Lựa chọn kích thước bản đồ sẵn có nhập 0, tự nhập kích thước chọn 1, quay lại màn hình chính chọn 2 \n";
+		cin >> choice;
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(1000,'\n');
+			choice = -1;
+			cout << "Lựa chọn của bạn chưa chính xác vui lòng nhập lại lựa chọn của bạn: ";
+		}
+	} while (choice > 2 || choice < 0);
+	if (choice == 0)
+	{
+		do
+		{
+			cout << "Kích thước bản đồ tự chọn gồm có 0: 3x3, 1: 5x5, 2: 10x10, 3: 15x15, 4: 20x20 \n";
+			cin >> choice;
+			if (cin.fail())
+			{
+				cin.clear();
+				cin.ignore(1000, '\n');
+				choice = -1;
+				cout << "Lựa chọn của bạn chưa chính xác vui lòng nhập lại lựa chọn của bạn: \n";
+			}
+			else if(choice == 0)
+			{
+				a.col = 4;
+				a.row = 4;
+			}
+			else if(choice == 1)
+			{
+				a.col = 6;
+				a.row = 6;
+			}
+			else if(choice == 2)
+			{
+				a.col = 11;
+				a.row = 11;
+			}
+			else if(choice==3)
+			{ 
+				a.col = 16;
+				a.row = 16;
+			}
+			else
+			{
+				a.col = 21;
+				a.row = 21;
+			}
+			
+		} while (choice < 0 || choice > 4);
+		choice = -1;
+	}
+	else if (choice == 1)
+	{
+			cout << "Nhập kích thước mà bạn mong muốn \n";
+			do
+			{
+				cout << "Nhập độ dài hàng: ";
+				cin >> a.row;
+				if (cin.fail())
+				{
+					cin.clear();
+					cin.ignore(1000, '\n');
+					a.row = -2;
+					cout << "Độ dài hàng bạn nhập chưa đúng vui lòng nhập lại: \n";
+				}
+				a.row++;
+			} while (a.row < 0);
+			do
+			{
+				cout << "Nhập độ dài cột: ";
+				cin >> a.col;
+				if (cin.fail())
+				{
+					cin.clear();
+					cin.ignore(1000, '\n');
+					a.col = -2;
+					cout << "Độ dài cột bạn nhập chưa đúng vui lòng nhập lại : \n";
+				}
+				a.col++;
+			} while (a.col < 0);
+	}
+	else
+	{
+		return;
+	}
 	arr = new string * [a.row];
 	for (int i = 0; i < a.row; i++)
 	{
@@ -210,11 +301,9 @@ void deleteArray(string** arr,mapsize a)
 
 int main()
 {
-	mapsize a;
-	a.col = 6;
-	a.row = 6;
+	mapsize a = { 0,0 };
 	string** arr=nullptr;
-	creatmap(arr, a);
+	creatmap("Người chơi lựa chọn chọn kích thước bản đồ hoặc tự nhập kích thước :\n", arr, a);
 
 
 
